2fc5b0045cd18c4a01d6fb7095e7679019dcff0e
JTMassa <tawejulius1975@gmail.com>
2025-12-17 17:13:44 -0500

    AlB testing

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket       = "w7-julio-terra-project"
     key          = "week10/terraform.tfstate"
     region       = "us-east-1"
-    use_lockfile = false
+    //use_lockfile = false
   }
 }
